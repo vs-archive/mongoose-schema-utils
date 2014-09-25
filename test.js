dbf0e4cc773a7df2d21b268377d68d33d7ecff47
Dmitriy Shekhovtsov <valorkin@gmail.com>
2014-09-25 17:45:11 +0300

    inherit parent options

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ var Mixed = mongoose.Schema.Types.Mixed;
 var sub = {
   // simple types
   str: String, strType: {type: String, enum: ['on', 'off']},
-  num: Number, numType: {type: String, some: true},
+  num: Number, numType: {type: Number, some: true, some1: {prop: 'test'}, private: false},
   bool: Boolean, boolType: {type: Boolean, some: true},
   arr: [], arrType: {type:[]},
   //buf: Buffer, bufType: {type: Buffer},
@@ -20,7 +20,7 @@ var sub = {
 var subSchema = new Schema({
   // simple types
   str: String, strType: {type: String, enum: ['on', 'off']},
-  num: Number, numType: {type: String, some: true},
+  num: Number, numType: {type: Number, some: true, some1: {prop: 'test'}, private: false},
   bool: Boolean, boolType: {type: Boolean, some: true},
   arr: [], arrType: {type:[]},
   //buf: Buffer, bufType: {type: Buffer},
@@ -33,26 +33,26 @@ var subSchema = new Schema({
 
 var schema = new Schema({
   // exception
-  type: {type: String, enum: ['on', 'off']},
-  // nested type with mixed types
-  nested: { file: {name:String, image: {type:String}}},
-  // simple types
-  str: String, strType: {type: String, enum: ['on', 'off']},
-  num: Number, numType: {type: String, some: true},
-  bool: Boolean, boolType: {type: Boolean, some: true},
-  arr: [], arrType: {type:[]},
-  simpleArr: [String], simpleArrType: {type:[String]},
-  //buf: Buffer, bufType: {type: Buffer},
-  date: Date, dateType: {type: Date, some: true},
-  objId: ObjectId, objIdType: { type: ObjectId, ref: 'Users'},
-  // mixed types
-  mix: Mixed, mixType: { type: Mixed },
-  obj: {}, objType: { type: {}},
-  sub: sub, subType: {type: sub},
+  //type: {type: String, enum: ['on', 'off']},
+  //// nested type with mixed types
+  //nested: { file: {name:String, image: {type:String}}},
+  //// simple types
+  //str: String, strType: {type: String, enum: ['on', 'off']},
+  //num: Number, numType: {type: String, some: true},
+  //bool: Boolean, boolType: {type: Boolean, some: true},
+  //arr: [], arrType: {type:[]},
+  //simpleArr: [String], simpleArrType: {type:[String]},
+  ////buf: Buffer, bufType: {type: Buffer},
+  //date: Date, dateType: {type: Date, some: true},
+  //objId: ObjectId, objIdType: { type: ObjectId, ref: 'Users'},
+  //// mixed types
+  //mix: Mixed, mixType: { type: Mixed },
+  //obj: {}, objType: { type: {}},
+  sub: sub, subType: {type: sub, private: true, some1: {prop: true}},
   // mixed type
-  subArr: [sub], subArrType: { type: [sub] },
-  // sub schema type
-  subSch: [subSchema], subSchType: {type: [subSchema]}
+  //subArr: [sub], subArrType: { type: [sub] },
+  //// sub schema type
+  //subSch: [subSchema], subSchType: {type: [subSchema]}
   // schema can not be a type
   //subSchema: subSchema, subSchemaType: {type: subSchema},
 });
